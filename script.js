0643dc5e2905594641b94ab9d92395a8633aaaef
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-07-12 23:17:43 +0530

    first commit

--- script.js
+++ script.js
@@ -103,3 +103,26 @@
 // clickHandler();
 // clickHandler();
 // clickHandler();
+
+function createToaster(config) {
+  return function (notification) {
+    let div = document.createElement("div");
+    div.className = `toaster ${config.theme === "dark" ? " bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 round shadow-lg pointer-event-none ${config.positionX === "right" ? "right-10" : "left-10"} ${config.positionY === "top" ? "top-10" : "bottom-10"}`;
+
+    div.textContent = notification;
+    document.body.appendChild(div);
+
+    setTimeout(() => {
+      document.body.removeChild(div);
+    }, config.duration * 1000);
+  };
+}
+
+let toaster = createToaster({
+  positionX: "left",
+  positionY: "top",
+  theme: "light",
+  duration: 3,
+});
+
+toaster("This is a dummy notification !")
